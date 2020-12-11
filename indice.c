b930bcd094e81670f8f1306d9b11a00f56a895e7
FilipeFerracini <58984299+FilipeFerracini@users.noreply.github.com>
2020-12-11 11:20:54 -0400

    update estético

--- indice.c
+++ indice.c
@@ -15,22 +15,53 @@ typedef struct{
     int rrn;
 }tpos;
 
+void troca(int *a, int *b){
+    int aux;
+    aux=*a;
+    *a=*b;
+    *b=aux;
+}
+
+int separa(int *v, int comeco, int fim){
+    int pivo=v[fim];
+    int j=comeco,k;
+    for(k=comeco;k<fim;k++){
+        if(v[k]<=pivo){
+            troca(&v[k],&v[j]);
+            j++;
+        }
+    }
+    troca(&v[j],&v[fim]);
+    return j;
+}
+
+void quicksort(int *v,int comeco, int fim){
+    int pivo;
+    if(comeco<fim){
+        pivo=separa(v,comeco,fim);
+        quicksort(v,comeco,pivo-1);
+        quicksort(v,pivo+1,fim);
+    }
+}
+
 int main(void){
     FILE * arq, *fout;
-    tpos vec[1000000];
+    tpos vec[1000001];
     ttipo car;
+    int i;
     arq=fopen("base.bin","rb");
     fout=fopen("index.bin","wb");
-    fread(&car, sizeof(char),sizeof(ttipo),arq);
-    strcpy(vec[0].placa,car.placa);
-    vec[0].rrn=1;
-    printf("%s %d\n",vec[0].placa,vec[0].rrn);
-    for(int i=0;i<5;i++){
+
+    for(i=0;i<=1000000;i++){
         fread(&car, sizeof(char),sizeof(ttipo),arq);
-        printf("%s\n", car.placa);
-    }
+        strcpy(vec[i].placa,car.placa);
+        vec[i].rrn=i+1;}
+        
+    i--;
     fclose(arq);
     fclose(fout);
-
+    printf("%d\n",i);
+    printf("%s %d\n",vec[0].placa,vec[0].rrn);
+    printf("%s %d\n",vec[i].placa,vec[i].rrn);
     return 0;
 }
